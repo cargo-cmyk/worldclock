c4b65cfb1a0f587f6b8e4e284fcbfa43f4778a6e
cargo-cmyk <carolin.goetz@arbor-insight.com>
2025-12-18 18:27:00 +0100

    add date and time for current cities + css change

--- src/index.js
+++ src/index.js
@@ -21,3 +21,20 @@ citySelector.addEventListener("change", function () {
     alert(`It is ${formattedDate} in ${timezone}`);
   }
 });
+
+function updateCityTime() {
+  // Los Angeles
+  let laTime = moment().tz("America/Los_Angeles");
+  document.querySelector("#la-date").innerHTML = laTime.format("MMMM Do YYYY");
+  document.querySelector("#la-time").innerHTML = laTime.format("h:mm:ss A");
+
+  // Sydney
+  let sydneyTime = moment().tz("Australia/Sydney");
+  document.querySelector("#sydney-date").innerHTML =
+    sydneyTime.format("MMMM Do YYYY");
+  document.querySelector("#sydney-time").innerHTML =
+    sydneyTime.format("h:mm:ss A");
+}
+
+updateCityTime();
+setInterval(updateCityTime, 1000);
